fc2c5447b840b31dfb9f9032d789264cea084d74
Karan <Kumarkay.ca@gmail.com>
2020-01-14 16:03:28 -0500

    init

---
+++ iOS_iPad_Trail/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  iOS_iPad_Trail
+//
+//  Created by MacStudent on 2020-01-14.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var webView: WKWebView!
+    
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail["url"].stringValue
+                if let url = URL(string: detail["url"].stringValue){
+                    let request  = URLRequest(url: url)
+                    webView.load(request)
+                }
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: JSON? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
